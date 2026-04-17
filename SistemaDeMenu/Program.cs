75c7c833951f6149321553dbc5b1c9414ab8051f
igorfriese <igorfriese2000@gmail.com>
2026-04-16 21:46:43 -0300

    adiciona buscador de nomes

--- SistemaDeMenu/Program.cs
+++ SistemaDeMenu/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
+            string[] nome = { "João", "Maria", "Pedro" };
 
+            Console.WriteLine("===MENU===");
+            string opcao = "";
+
+            while (opcao.ToLower() != "x")
+            {
+                Console.Write("3 - Buscar um nome \nx - Sair do programa \nDigite uma opção: ");
+                opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "3":
+                        Console.Write("Digite o nome que deseja buscar: ");
+                        string nomeBusca = Console.ReadLine();
+                        bool encontrado = false;
+
+                        for (int i = 0; i < nome.Length; i++)
+                        {
+                            if (nomeBusca.ToLower() == nome[i].ToLower())
+                            {
+                                Console.WriteLine($"Nome encontrado: {nome[i]}\n");
+                                encontrado = true;
+                            }
+                        }
+                        if (encontrado == false)
+                        {
+                            Console.WriteLine("Nome não encontrado!\n");
+                        }
+                        break;
+
+                    case "x":
+                        Console.WriteLine("Saindo...");
+                        break;
+
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
